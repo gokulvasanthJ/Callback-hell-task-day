c8ff216e1fc17c1a57bc8b18f6ec81656a80ccfa
gokulvasanthJ <gokul27mech@gmail.com>
2023-04-19 18:39:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+
+function foo(){
+    setTimeout(function timer10 (){
+    console.log(10)},1000);
+    
+    setTimeout(function timer9(){
+    console.log(9)
+    },2000)
+    
+    setTimeout(function timer8(){
+    console.log(8)
+    },3000)
+    
+    setTimeout(function timer7(){
+    console.log(7)
+    },4000);
+    
+    setTimeout(function timer6(){
+    console.log(6)
+    },5000);
+    
+    setTimeout(function timer5(){
+    console.log(5)
+    },6000);
+    
+    setTimeout(function timer4(){
+    console.log(4)
+    },7000);
+    
+    setTimeout(function timer3(){
+    console.log(3)
+    },8000);
+    
+    setTimeout(function timer2(){
+    console.log(2)
+    },9000);
+    
+    setTimeout(function timer1(){
+    console.log(1)
+    },10000);
+    
+    setTimeout(function timertext(){
+    console.log("Happy Independence Day")
+    },11000)
+    
+}
+   foo()
+
+    // document.getElementById("timer").innerText=timerfunction
+    // document.querySelector("#timer").style.colcor="green"
+
